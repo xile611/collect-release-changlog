563a19a01fed88b14f7ecbe0efa00345d0266a7c
xile611 <dingling112@gmail.com>
2023-12-19 17:04:04 +0800

    feat: update the format of changelog

--- src/release.ts
+++ src/release.ts
@@ -1,6 +1,7 @@
 import * as github from '@actions/github'
 import * as core from '@actions/core'
 import { writeFileSync, readFileSync, existsSync } from 'fs'
+import semver from 'semver'
 
 export async function updateOrAppendChanglog(
   files: string[],
@@ -24,7 +25,9 @@ export async function updateOrAppendChanglog(
   return result
 }
 
-function formatReleaseMarkdown(releaseMarkdown: string | null | undefined) {
+function formatReleaseMarkdown(
+  releaseMarkdown: string | null | undefined
+): string {
   if (!releaseMarkdown) {
     return ''
   }
@@ -48,20 +51,39 @@ function formatReleaseMarkdown(releaseMarkdown: string | null | undefined) {
   return output
 }
 
+function sortReleases<T extends { tag_name: string }>(data: T[]): T[] {
+  if (!data || !data.length) {
+    return [] as T[]
+  }
+
+  const filterData = data.filter(entry =>
+    semver.valid(entry.tag_name.replace('v', ''))
+  )
+
+  filterData.sort((a: T, b: T) => {
+    return semver.gt(a.tag_name.replace('v', ''), b.tag_name.replace('v', ''))
+      ? 1
+      : -1
+  })
+
+  return filterData
+}
+
 async function initChangelog(file: string): Promise<boolean> {
   const githubToken = core.getInput('token')
   const octokit = github.getOctokit(githubToken)
   const { owner, repo } = github.context.repo
   const releases = await octokit.rest.repos.listReleases({ owner, repo })
+  const releaseData = sortReleases(releases.data)
 
   let changelog = ''
   let bodyStr = ''
 
-  for (const release of releases.data) {
+  for (const release of releaseData) {
     bodyStr = formatReleaseMarkdown(release.body)
 
     if (bodyStr) {
-      changelog += `## ${release.tag_name}\n\n${bodyStr}\n\n`
+      changelog += `## ${release.tag_name}\n* Release Time: ${release.published_at}\n\n${bodyStr}\n[more detail about${release.tag_name}](${release.url})\n\n`
     }
   }
 
@@ -85,11 +107,14 @@ async function appendChangelog(file: string, tag: string): Promise<boolean> {
 
   try {
     const data = readFileSync(file, 'utf8')
+
     writeFileSync(
       file,
-      `## ${release.data.tag_name}\n\n${formatReleaseMarkdown(
-        release.data.body
-      )}\n\n${data}`
+      `## ${release.data.tag_name}\n* Release Time: ${
+        release.data.published_at
+      }\n\n${formatReleaseMarkdown(release.data.body)}\n[more detail about${
+        release.data.tag_name
+      }](${release.url})\n\n${data}`
     )
 
     return true
